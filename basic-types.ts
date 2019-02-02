86865dc23ea90744fca83c4561f199a9c7553465
Hector Daniel Gameros <hectordgamerosm@gmail.com>
2019-02-02 01:39:42 -0800

    advanced types

--- basic-types.ts
+++ basic-types.ts
@@ -1,2 +1,10 @@
 // Array
-let list: number[] = [ 1, 2, 3 ];
+// let list: number[] = [ 1, 2, 3 ];
+// let list: Array<number> = [ 1, 2, 3 ];
+
+// Tuple
+let x: [ string, number ];
+x = [ 'hello', 10 ];
+// x = [ 10, 'hello', 4, 4, 4 ];
+// 
+// x[ 3 ] = 'world'; does not work, even though the documentation says so
