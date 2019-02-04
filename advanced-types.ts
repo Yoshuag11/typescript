5cbdd23ef8a9f6fa2d55321c680c29c18581aac6
Hector Daniel Gameros <hectordgamerosm@gmail.com>
2019-02-03 23:22:16 -0800

    Finished basic types

--- advanced-types.ts
+++ advanced-types.ts
@@ -1,3 +1,18 @@
+// Intersection Types
+// example of a mixin
+function extend<T, U> ( first: T, second: U ): T & U {
+	let result = <T & U>{};
+
+	for ( let id in first ) {
+		( <any>result )[ id ] = ( <any>first )[ id ];
+	}
+	for ( let id in second ) {
+		if ( !result.hasOwnProperty( id ) ) {
+			( <any>result )[ id ] = ( <any>second )[ id ];
+		}
+	}
+	return result;
+}
 // Union Types
 // function padLeft ( value: string, padding: any ) {
 // 	if ( typeof padding === 'number' ) {
